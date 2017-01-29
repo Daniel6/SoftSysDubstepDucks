16d40e2cc1949de420ba41841097f32fa960e03e
Thuc Tran <thuc.tran@students.olin.edu>
2017-01-29 17:23:04 -0500

    move things to examples folder, for real

---
+++ examples/p2p-chat-example/message.h
@@ -0,0 +1,13 @@
+// https://github.com/bschmaltz/c-p2p-chat/blob/master/message.h
+
+typedef struct message_header_t {
+  char type;
+  char error;
+  unsigned int room;
+  unsigned int payload_length;
+} message_header;
+
+typedef struct packet_t {
+  struct message_header_t header;
+  char payload[1000];
+} packet;
